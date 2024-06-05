6a94bb6adb0d5f173ccc58fd70c6b9d8bb821fb0
toliguemi <toliguemi94@gmail.com>
2024-06-05 16:24:35 -0400

    comment change

--- ReuPlan_Django/settings.py
+++ ReuPlan_Django/settings.py
@@ -32,8 +32,6 @@ if __name__ == "__main__":
 
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
